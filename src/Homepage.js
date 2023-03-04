e69ef3528ba6cd507b74ad5dfcbc68719afbb34d
Sid <siddhant997@gmail.com>
2023-03-03 23:12:24 -0500

    changes for minimal reproducible example

--- src/Homepage.js
+++ src/Homepage.js
@@ -1,27 +1,72 @@
 import React, {useState} from 'react';
-import text_writer from './text_writer.js';
-import profile from './n.jpg';
 export default function Homepage()  {
-    const [loaded, setLoaded] = useState(true);
+    const [loaded, setLoaded] = useState(false);
     const lines = ["programs.","blogs.","poems.", "stories."];
-    text_writer();
+    
+    
+     function text_writer(){
+        var TxtRotate = function(el, toRotate, period) {
+            this.toRotate = toRotate;
+            this.el = el;
+            this.loopNum = 0;
+            this.period = parseInt(period, 10) || 2000;
+            this.txt = '';
+            this.tick();
+            this.isDeleting = false;
+          };
+          
+          TxtRotate.prototype.tick = function() {
+            var i = this.loopNum % this.toRotate.length;
+            var fullTxt = this.toRotate[i];
+          
+            if (this.isDeleting) {
+              this.txt = fullTxt.substring(0, this.txt.length - 1);
+            } else {
+              this.txt = fullTxt.substring(0, this.txt.length + 1);
+            }
+          
+            this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+          
+            var that = this;
+            var delta = 300 - Math.random() * 100;
+          
+            if (this.isDeleting) { delta /= 2; }
+          
+            if (!this.isDeleting && this.txt === fullTxt) {
+              delta = this.period;
+              this.isDeleting = true;
+            } else if (this.isDeleting && this.txt === '') {
+              this.isDeleting = false;
+              this.loopNum++;
+              delta = 500;
+            }
+          
+            setTimeout(function() {
+              that.tick();
+            }, delta);
+          };
+          window.onload = function() {
+            var elements = document.getElementsByClassName('txt-rotate');
+            for (var i=0; i<elements.length; i++) {
+              var toRotate = elements[i].getAttribute('data-rotate');
+              var period = elements[i].getAttribute('data-period');
+              if (toRotate) {
+                new TxtRotate(elements[i], toRotate.split(","), period);
+              }
+            }
+            // INJECT CSS
+            var css = document.createElement("style");
+            css.type = "text/css";
+            css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+            document.body.appendChild(css);
+          };
+        }
+        text_writer();
+
     return(
         <>
-            {
-            loaded ? 
-            <div 
-                className='fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-white flex flex-col items-center justify-center'
-                >
-                    Loading...
-                </div>
-                : null
-        }
         
         <div id="home" className={`flex w-full h-screen gap-5 items-center justify-center relative flex-col md:flex-row  `}>
-            
-            <div className='w-16 md:w-32 lg:w-48'>
-                <img data-aos="flip-right" data-aos-duration="1500" data-aos-offset="200" className='rounded-full' src={profile} onLoad={() => setLoaded(false)}/>
-            </div> 
             
             <div className='md:w-3/6' data-aos="fade-right" data-aos-duration="1000" data-aos-offset="100" >
             <link href="https://fonts.googleapis.com/css?family=Raleway:200,100,400" rel="stylesheet" type="text/css" />
